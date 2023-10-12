ad43b7aa7aef78ce486c56447c5569a84747ec52
duynhannguyen <130212806+duynhannguyen@users.noreply.github.com>
2023-10-13 01:53:44 +0700

    create UI login page

---
+++ src/componets/FieldTextInput.js/FieldTextInput.jsx
@@ -0,0 +1,35 @@
+import { EyeInvisibleOutlined, EyeOutlined } from "@ant-design/icons";
+import React, { useState } from "react";
+const FieldTextInput = ({
+  id,
+  label,
+  placeHolder,
+  showPassword,
+  setShowToggleIcon,
+  ...restProps
+}) => {
+  const inputProps = {
+    id,
+    placeHolder,
+    showPassword,
+    setShowToggleIcon,
+    ...restProps,
+  };
+  return (
+    <div className="relative">
+      <input
+        {...inputProps}
+        onChange={(e) => setShowToggleIcon(e.target.value)}
+        className="w-full py-2 px-3 text-gray-700 bg-white border-b-2 outline-0 focus:border-b-yellow-700 mb-3 "
+      ></input>
+      {/* <span
+        className="absolute right-1 top-2 cursor-pointer"
+        onClick={() => setShowPassword(!showPassword)}
+      >
+        {showPassword ? <EyeOutlined /> : <EyeInvisibleOutlined />}
+      </span> */}
+    </div>
+  );
+};
+
+export default FieldTextInput;
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,55 @@
+import { EyeInvisibleFilled, EyeOutlined } from "@ant-design/icons";
+import React, { useState } from "react";
+import Button from "../../componets/Button.jsx/Button.jsx";
+import FieldTextInput from "../../componets/FieldTextInput.js/FieldTextInput.jsx";
+
+const LoginPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showToggleIcon, setShowToggleIcon] = useState("");
+  const setInputType = (showPassword) => {
+    if (showPassword) {
+      return "text";
+    } else {
+      return "password";
+    }
+  };
+  const inputType = setInputType(showPassword);
+  return (
+    <div className="w-full h-ful relative ">
+      <img
+        className=" w-full max-h-screen opacity-10  "
+        src="images/loginbg.jpg"
+        alt="background"
+      />
+      <div className=" text-center w-[440px] h-auto absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 bg-white text-black rounded-lg p-2   ">
+        <p className="font-extrabold text-2xl mt-4 mb-2 text-yellow-700">
+          MAYME
+        </p>
+        <p className=" text-xl mb-3 font-bold ">Đăng nhập</p>
+        <form>
+          <FieldTextInput placeHolder={"Tên đăng nhập"} />
+          <FieldTextInput
+            placeHolder={"Mật khẩu"}
+            type={inputType}
+            showPassword={showPassword}
+            value={showToggleIcon}
+            setShowToggleIcon={setShowToggleIcon}
+          />
+          <Button name={"Đăng nhập"} />
+          {showToggleIcon ? (
+            <span
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-[20px] bottom-[88px] cursor-pointer"
+            >
+              {showPassword ? <EyeOutlined /> : <EyeInvisibleFilled />}
+            </span>
+          ) : (
+            ""
+          )}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;

767fd08e8b6fa8c55ac3365b80269bde9e0e9108
duynhannguyen <130212806+duynhannguyen@users.noreply.github.com>
2023-10-17 04:51:37 +0700

    handle function logout

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,20 +1,54 @@
 import React from "react";
-import { AiOutlineUser } from "react-icons/ai";
-import { useSelector } from "react-redux";
+import { UserOutlined, LoginOutlined } from "@ant-design/icons";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../redux/Auth/authSlice.js";
+import { useNavigate } from "react-router-dom";
 
 const MainPage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const currentUser = useSelector((state) => state.auth.currentUser);
   console.log(currentUser);
-
+  const onHandleLogout = () => {
+    dispatch(logout());
+    navigate("/");
+  };
   return (
     <div>
       <div className="flex justify-between items-center mb-4 container mx-auto px-4 py-2">
         <h1 className="text-4xl font-bold text-yellow-700">MAYME</h1>{" "}
-        <div className="flex items-center">
+        <div className=" border-2 border-yellow-700 w-auto p-4 rounded-2xl flex items-center hover:bg-amber-700 transition-all duration-300 relative group cursor-default justify-center text-lg ">
           <div className="text-right mr-2">{currentUser.tenNhaHang}</div>
-          <AiOutlineUser className="text-3xl" />
+          <UserOutlined />
+          <ul className="p-2 w-[150px] h-auto rounded-md opacity-0  group-hover:opacity-100 transition-all duration-300  absolute -bottom-[90%] z-10 text-black text-[13px] bg-white">
+            <li
+              onClick={onHandleLogout}
+              className="flex justify-center gap-2 items-center cursor-pointer "
+            >
+              <span> Đăng xuất </span>
+              <LoginOutlined />
+            </li>
+          </ul>
         </div>
       </div>
+
+      {/* <div class="relative group">
+        <div class="w-32 h-32 bg-gray-200 hover:bg-gray-300 transition-all duration-300"></div>
+        <ul class="opacity-0 group-hover:opacity-100 absolute top-full left-0 bg-white border border-gray-300 py-2 px-4 shadow z-10 transition-opacity duration-300">
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+      </div> */}
+      {/* <div class="relative group">
+        <div class="w-32 h-32 bg-gray-200 hover:bg-gray-300 transition-all duration-300"></div>
+        <ul class="opacity-100 group-hover:opacity-0 absolute top-full left-0 bg-white border border-gray-300 py-2 px-4 shadow z-10 transition-opacity duration-1000">
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+      </div> */}
+
       <div className="container mx-auto px-8 py-4">
         <div className="flex justify-between items-center mb-4">
           <h1 className="text-2xl font-bold">Hàng Hoá</h1>
--- src/redux/Auth/authSlice.js
+++ src/redux/Auth/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { TOKEN_TYPES } from "../../constant/constant";
 
 const initialState = {
   isAuthenticated: false,
@@ -15,9 +16,14 @@ const authSlice = createSlice({
       state.currentUser = payload.user;
       state.isAuthenticated = true;
     },
+    logout: (state) => {
+      localStorage.removeItem(TOKEN_TYPES.ACCESS_TOKEN);
+      state.isAuthenticated = false;
+      state.currentUser = {};
+    },
   },
 });
 
-export const { login } = authSlice.actions;
+export const { login, logout } = authSlice.actions;
 
 export default authSlice.reducer;

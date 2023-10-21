17b734c6c7d214028653dfe8ab4ef2d4627c231a
duynhannguyen <130212806+duynhannguyen@users.noreply.github.com>
2023-10-22 03:44:25 +0700

    merge from hoang-branch

--- src/component/AddProductForm/AddProductForm.jsx
+++ src/component/AddProductForm/AddProductForm.jsx
@@ -1,30 +1,29 @@
-import React from 'react';
-import { PhotoIcon } from '@heroicons/react/24/solid';
-import { useFormik } from 'formik';
+import React from "react";
+import { PhotoIcon } from "@heroicons/react/24/solid";
+import { useFormik } from "formik";
 
 const AddProductForm = ({ onSubmitHandler, onHandleCloseForm }) => {
   const formik = useFormik({
     initialValues: {
-      maHangHoa: '',
-      tenHang: '',
-      nhomHang: '',
-      loai: '',
-      giaBan: '',
-      giaVon: '',
+      maHangHoa: "",
+      tenHang: "",
+      nhomHang: "",
+      loai: "",
+      giaBan: "",
+      giaVon: "",
     },
     onSubmit: (values) => {
-      onSubmitHandler(values)
+      onSubmitHandler(values);
     },
   });
 
   const { handleSubmit, handleChange } = formik;
 
-
   return (
-    <form onSubmit={handleSubmit}>
-      <div className=" bg-white px-10 on " >
-        <div className="border-b     mt-20">
-          <h2 className="text-base font-semibold leading-7 pt-10" >Thêm hàng hóa</h2>
+    <form className="bg-white px-10 pt-6" onSubmit={handleSubmit}>
+      <div className="   ">
+        <div className="border-b">
+          <h2 className="text-base font-semibold leading-7">Thêm hàng hóa</h2>
           <div className="flex justify-start gap-40">
             <p>Thông tin</p>
             <p>Mô tả chi tiết</p>
@@ -33,7 +32,10 @@ const AddProductForm = ({ onSubmitHandler, onHandleCloseForm }) => {
         </div>
         <div className="grid grid-cols-3 gap-3 mt-2">
           <div className="col-span-2">
-            <label htmlFor="maHangHoa" className="block text-sm font-medium leading-6">
+            <label
+              htmlFor="maHangHoa"
+              className="block text-sm font-medium leading-6"
+            >
               Mã hàng hóa
             </label>
 
@@ -48,7 +50,10 @@ const AddProductForm = ({ onSubmitHandler, onHandleCloseForm }) => {
             />
           </div>
           <div>
-            <label htmlFor="giaVon" className="block text-sm font-medium leading-6">
+            <label
+              htmlFor="giaVon"
+              className="block text-sm font-medium leading-6"
+            >
               Giá vốn
             </label>
 
@@ -65,7 +70,10 @@ const AddProductForm = ({ onSubmitHandler, onHandleCloseForm }) => {
         </div>
         <div className="grid grid-cols-3 gap-3 mt-2">
           <div className="col-span-2">
-            <label htmlFor="tenHang" className="block text-sm font-medium leading-6">
+            <label
+              htmlFor="tenHang"
+              className="block text-sm font-medium leading-6"
+            >
               Tên hàng
             </label>
 
@@ -80,7 +88,10 @@ const AddProductForm = ({ onSubmitHandler, onHandleCloseForm }) => {
             />
           </div>
           <div>
-            <label htmlFor="giaBan" className="block text-sm font-medium leading-6">
+            <label
+              htmlFor="giaBan"
+              className="block text-sm font-medium leading-6"
+            >
               Giá bán
             </label>
 
@@ -95,7 +106,6 @@ const AddProductForm = ({ onSubmitHandler, onHandleCloseForm }) => {
             />
           </div>
         </div>
-
 
         <label htmlFor="loai" className="block text-sm font-medium leading-6">
           Loại thực đơn
@@ -113,11 +123,13 @@ const AddProductForm = ({ onSubmitHandler, onHandleCloseForm }) => {
             <option>Lựa chọn</option>
             <option>Đồ ăn</option>
             <option>Thức uống</option>
-
           </select>
         </div>
 
-        <label htmlFor="nhomHang" className="block text-sm font-medium leading-6">
+        <label
+          htmlFor="nhomHang"
+          className="block text-sm font-medium leading-6"
+        >
           Nhóm hàng
         </label>
         <div className="mt-2">
@@ -134,51 +146,60 @@ const AddProductForm = ({ onSubmitHandler, onHandleCloseForm }) => {
             <option>1</option>
             <option>2</option>
             <option>3</option>
-
           </select>
-
         </div>
 
         <div className="sm:col-span-6">
-          <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 mt-2">
+          <label
+            htmlFor="cover-photo"
+            className="block text-sm font-medium leading-6 mt-2"
+          >
             chọn ảnh
           </label>
           <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
             <div className="text-center">
-              <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+              <PhotoIcon
+                className="mx-auto h-12 w-12 text-gray-300"
+                aria-hidden="true"
+              />
               <div className="mt-4 flex text-sm leading-6 text-gray-600">
                 <label
                   htmlFor="file-upload"
                   className="relative cursor-pointer rounded-mdbg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
                 >
                   <span>Upload a file</span>
-                  <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                  <input
+                    id="file-upload"
+                    name="file-upload"
+                    type="file"
+                    className="sr-only"
+                  />
                 </label>
                 <p className="pl-1">or drag and drop</p>
               </div>
-              <p className="text-xs leading-5">PNG, JPG, GIF up to 10MB</p>
+              <p className="text-xs leading-5">PNG, JPG, GIF</p>
             </div>
           </div>
         </div>
 
         <div className="mt-6 mb-6  flex items-center justify-end gap-x-6">
-          <button type="button" onClick={onHandleCloseForm} className="text-sm font-semibold leading-6">
+          <button
+            type="button"
+            onClick={onHandleCloseForm}
+            className="text-sm font-semibold leading-6"
+          >
             Hủy
           </button>
           <button
             type="submit"
             className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-
           >
             Thêm
           </button>
         </div>
-
-
       </div>
     </form>
+  );
+};
 
-  )
-}
-
-export default AddProductForm
+export default AddProductForm;
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
+import { UserOutlined, LoginOutlined } from "@ant-design/icons";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../redux/Auth/authSlice.js";
+import { useNavigate } from "react-router-dom";
 import { AiOutlineUser } from "react-icons/ai";
 import axios from "axios";
 import AddProductForm from "../../component/AddProductForm/AddProductForm.jsx";
 import ProductAPI from "../../services/productAPI.js";
 const MainPage = () => {
   const [showAddProductForm, setShowAddProductForm] = useState(false);
-
-  const handleButtonClick = () => {
-    setShowAddProductForm(true);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const currentUser = useSelector((state) => state.auth.currentUser);
+  const onHandleLogout = () => {
+    dispatch(logout());
+    navigate("/");
   };
-
   const handleSubmit = async (values) => {
     console.log("values ne", values);
     try {
@@ -20,47 +26,71 @@ const MainPage = () => {
       console.error(error);
     }
   };
-
   const onHandleCloseForm = () => {
     setShowAddProductForm(false);
   };
-
+  const handleButtonClick = () => {
+    setShowAddProductForm(true);
+  };
   return (
     <div className="flex flex-col items-center  min-h-screen">
       <div className="flex justify-between items-center mb-4 container mx-auto px-4 py-2">
         <h1 className="text-4xl font-bold text-yellow-700">Mayme</h1>
-        <div className="flex items-center">
-          <div className="text-right mr-2">+84 9325374829</div>
-          <AiOutlineUser className="text-" />
+        <div className=" border-2 border-yellow-700 w-auto p-4 rounded-2xl flex items-center hover:bg-amber-700 transition-all duration-300 relative group cursor-default justify-center text-lg ">
+          <div className="text-right mr-2">{currentUser.tenNhaHang}</div>
+          <UserOutlined />
+          <ul className="p-2 w-[150px] h-auto rounded-md opacity-0  group-hover:opacity-100 transition-all duration-300  absolute -bottom-[90%] z-10 text-black text-[13px] bg-white">
+            <li
+              onClick={onHandleLogout}
+              className="flex justify-center gap-2 items-center cursor-pointer "
+            >
+              <span> Đăng xuất </span>
+              <LoginOutlined />
+            </li>
+          </ul>
         </div>
       </div>
+
+      {/* <div class="relative group">
+        <div class="w-32 h-32 bg-gray-200 hover:bg-gray-300 transition-all duration-300"></div>
+        <ul class="opacity-0 group-hover:opacity-100 absolute top-full left-0 bg-white border border-gray-300 py-2 px-4 shadow z-10 transition-opacity duration-300">
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+      </div> */}
+      {/* <div class="relative group">
+        <div class="w-32 h-32 bg-gray-200 hover:bg-gray-300 transition-all duration-300"></div>
+        <ul class="opacity-100 group-hover:opacity-0 absolute top-full left-0 bg-white border border-gray-300 py-2 px-4 shadow z-10 transition-opacity duration-1000">
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+      </div> */}
+
       <div className="container mx-auto px-8 py-4">
         <div className="flex justify-between items-center mb-4">
           <h1 className="text-2xl font-bold">Hàng Hoá</h1>
           <button
-            onClick={handleButtonClick}
             className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded"
+            onClick={handleButtonClick}
           >
             + Thêm Mới
           </button>
         </div>
-        <div className="grid grid-cols-6 gap-4 bg-blue-200">
-          <div className="col-span-1 p-2 mb-2 border-transparent">
-            Mã Hàng Hoá
-          </div>
-          <div className="col-span-1 p-2 mb-2 border-transparent">Tên Hàng</div>
-          <div className="col-span-1 p-2 mb-2 border-transparent">
-            Nhóm Hàng
-          </div>
-          <div className="col-span-1 p-2 mb-2 border-transparent">Loại</div>
-          <div className="col-span-1 p-2 mb-2 border-transparent">Giá Bán</div>
-          <div className="col-span-1 p-2 mb-2 border-transparent">Giá Vốn</div>
+        <div className="grid grid-cols-6 text-amber-300 border-2 border-sky-200 gap-4 bg-blue-400">
+          <div className="col-span-1 p-2  border-transparent">Mã Hàng Hoá</div>
+          <div className="col-span-1 p-2  border-transparent">Tên Hàng</div>
+          <div className="col-span-1 p-2  border-transparent">Nhóm Hàng</div>
+          <div className="col-span-1 p-2  border-transparent">Loại</div>
+          <div className="col-span-1 p-2  border-transparent">Giá Bán</div>
+          <div className="col-span-1 p-2  border-transparent">Giá Vốn</div>
         </div>
       </div>
 
       {showAddProductForm && (
         <div
-          className=" fixed top-,0 left-0 right-0 bottom-0 flex items-center justify-center  bg-opacity-40 bg-gray-400	"
+          className=" fixed min-h-screen top-,0 left-0 right-0 bottom-0 flex items-center justify-center  bg-opacity-40 bg-gray-400	"
           style={{ zIndex: 9999 }}
         >
           <AddProductForm

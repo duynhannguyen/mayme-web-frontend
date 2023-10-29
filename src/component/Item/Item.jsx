d1ffd05509a845fbb33ff143c2e0b67b115383b5
dhoangg003 <duonghoang0363181030@gmail.com>
2023-10-30 02:07:36 +0700

    listmenu update

--- src/component/Item/Item.jsx
+++ src/component/Item/Item.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
 
-const Item = () => {
+import React from 'react';
+
+const Item = (props) => {
+  const { imageUrl, title, description, price } = props;
+
   return (
-    
-      <div className=" p-4 rounded-lg shadow-md bg-gray-900 text-white">
-              <img
-                src="https://t2.gstatic.com/licensed-image?q=tbn:ANd9GcRqPnxhzG50sOFqBgyKvZtmOHiB3mwkR2YtId5jZG5nApAoiSDkXMK4Rxxqpkfg0ZW9"
-                alt="Cappuccino"
-                className="w-full  object-cover mb-4 rounded-lg"
-              />
-              <h3 className="text-xl font-semibold mb-2">Cappuccino</h3>
-              <p className="text-gray-600 mb-2">Mô tả về cappuccino.</p>
-              <span className="text-yellow-500 font-bold">$3.99</span>
-            </div>
-    
-  )
-}
+    <div className="p-4 rounded-lg shadow-md bg-gray-900 text-white">
+      <img
+        src={imageUrl}
+        alt={title}
+        className="w-full object-cover mb-4 rounded-lg"
+      />
+      <h3 className="text-xl font-semibold mb-2">{title}</h3>
+      <p className="text-gray-600 mb-2">{description}</p>
+      <span className="text-yellow-500 font-bold">{price}</span>
+    </div>
+  );
+};
 
-export default Item
+export default Item;
--- src/pages/ListMenu/ListMenu.jsx
+++ src/pages/ListMenu/ListMenu.jsx
@@ -1,12 +1,91 @@
-
 import React, { useState } from 'react';
 import Item from '../../component/Item/Item'
 import QRCode from 'qrcode.react';
 const ListMenu = () => {
   const menu2Url = 'http://localhost:5173/menu2';
-
   const [showQRCode, setShowQRCode] = useState(false);
+  const [currentMenu, setCurrentMenu] = useState('coffee');
+  const axios = require('axios');
 
+const fetchData = async () => {
+  try {
+    const response = await axios.get('http://localhost:5173/api/v1/mainpage/menu');
+    const data = response.data;
+    console.log(data); // Xử lý dữ liệu nhận được từ backend ở đây
+  } catch (error) {
+    console.log('Error fetching data:', error);
+  }
+};
+
+fetchData();
+  const dataCoffee = [
+      {
+      imageUrl:
+        'https://t2.gstatic.com/licensed-image?q=tbn:ANd9GcRqPnxhzG50sOFqBgyKvZtmOHiB3mwkR2YtId5jZG5nApAoiSDkXMK4Rxxqpkfg0ZW9',
+      title: 'Cappuccino',
+      description: 'Mô tả về cappuccino.',
+      price: '$3.99',
+    },
+  ];
+  const dataNuocep = [
+    {
+      imageUrl:
+        'https://t2.gstatic.com/licensed-image?q=tbn:ANd9GcRqPnxhzG50sOFqBgyKvZtmOHiB3mwkR2YtId5jZG5nApAoiSDkXMK4Rxxqpkfg0ZW9',
+      title: 'Nước ép cam',
+      description: 'Mô tả về Nước ép cam.',
+      price: '$3.99',
+    },
+  ];
+  const dataTrasua = [
+    {
+      imageUrl:
+        'https://t2.gstatic.com/licensed-image?q=tbn:ANd9GcRqPnxhzG50sOFqBgyKvZtmOHiB3mwkR2YtId5jZG5nApAoiSDkXMK4Rxxqpkfg0ZW9',
+      title: 'trà sữa đài loan',
+      description: 'Mô tả về trà sữa đài loan.',
+      price: '$3.99',
+    },
+   
+  ];
+  const dataSinhto = [
+    {
+      imageUrl:
+        'https://t2.gstatic.com/licensed-image?q=tbn:ANd9GcRqPnxhzG50sOFqBgyKvZtmOHiB3mwkR2YtId5jZG5nApAoiSDkXMK4Rxxqpkfg0ZW9',
+      title: 'sinh tố 1',
+      description: 'Mô tả về sinh tố.',
+      price: '$3.99',
+    },
+    {
+      imageUrl: 'https://t2.gstatic.com/licensed-image?q=tbn:ANd9GcRqPnxhzG50sOFqBgyKvZtmOHiB3mwkR2YtId5jZG5nApAoiSDkXMK4Rxxqpkfg0ZW9',
+      title: 'sinh to',
+      description: 'Mô tả về Latte.',
+      price: '$19.99',
+    },
+  ];
+  const handleMenuClick = (menu) => {
+    setCurrentMenu(menu);
+    setShowQRCode(false);
+  };
+  const renderMenuItems = () => {
+    let menuData = [];
+
+    if (currentMenu === 'coffee') {
+      menuData = dataCoffee;
+    } else if (currentMenu === 'nuocep') {
+      menuData = dataNuocep;
+    } else if (currentMenu === 'trasua') {
+      menuData = dataTrasua;
+    } else if (currentMenu === 'sinhto') {
+      menuData = dataSinhto;
+    }return menuData.map((item, index) => (
+      <Item
+        key={index}
+        imageUrl={item.hinhAnh}
+        title={item.tenHang}
+        description={item.nhomHang}
+        price={item.giaBan}
+      />
+    ));
+  };
   const handleShowQRCode = () => {
     setShowQRCode(true);
   };
@@ -28,55 +107,34 @@ const ListMenu = () => {
               </svg>
               Menu
             </a>
-
-            <a className="bg-gray-800 text-white p-4 inline-flex justify-center rounded-md" href="#">
-
+            <button className="bg-gray-800 text-white p-4 inline-flex justify-center rounded-md"   onClick={() => handleMenuClick('coffee')}>
               Coffee
-            </a>
-            <a className="text-white/50 p-4 inline-flex justify-center rounded-md hover:bg-gray-800 hover:text-white smooth-hover" href="#">
+            </button>
+            <button className="bg-gray-800 text-white p-4 inline-flex justify-center rounded-md"  onClick={() => handleMenuClick('trasua')}>
 
               Trà sữa
-            </a>
-            <a className="text-white/50 p-4 inline-flex justify-center rounded-md hover:bg-gray-800 hover:text-white smooth-hover" href="#">
-
+            </button>
+            <button className="bg-gray-800 text-white p-4 inline-flex justify-center rounded-md" onClick={() => handleMenuClick('sinhto')}>
               Sinh Tố
-            </a>
-            <a className="text-white/50 p-4 inline-flex justify-center rounded-md hover:bg-gray-800 hover:text-white smooth-hover" href="#">
-
+              </button>
+            <button className="bg-gray-800 text-white p-4 inline-flex justify-center rounded-md" onClick={() => handleMenuClick('nuocep')}>
               Nước ép
-            </a>
+            </button>
           </nav>
-
         </div>
-
         <div className="flex-1 px-2 sm:px-0">
           <div className="flex justify-between items-center">
             <h3 className="text-3xl font-extralight text-white/50">MayMe</h3>
             <div className="inline-flex items-center space-x-2">
            <button className="bg-gray-900 text-white/50 p-2 rounded-md hover:text-white smooth-hover"  onClick={handleShowQRCode}>
-            
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
-                </svg> 
-                
+               </svg>               
               </button>
-              
-             
-
             </div>
           </div>
           <div className="mb-10 sm:mb-0 mt-10 grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-
-
+          {renderMenuItems()}
           </div>
         </div>
       </div>
@@ -89,7 +147,6 @@ const ListMenu = () => {
     </div>
   )}
     </div>
-
   )
 }
 

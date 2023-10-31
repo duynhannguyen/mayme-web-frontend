9b70a0fc508a7ea4a205a3fc6bb809a2234038cd
dhoangg003 <duonghoang0363181030@gmail.com>
2023-10-31 11:45:56 +0700

    add to cart

--- src/component/Item/Item.jsx
+++ src/component/Item/Item.jsx
@@ -1,9 +1,12 @@
 
 import React from 'react';
 
-const Item = (props) => {
-  const { imageUrl, title, description, price } = props;
 
+
+    const Item = ({ imageUrl, title, description, price, onAddToCart }) => {
+      const handleAddToCart = () => {
+        onAddToCart({ imageUrl, title, description, price });
+      };
   return (
     <div className="p-4 rounded-lg shadow-md bg-gray-900 text-white">
       <img
@@ -11,9 +14,12 @@ const Item = (props) => {
         alt={title}
         className=" object-cover mb-4 rounded-lg w-48 h-52 "
       />
-      <h3 className="text-xl font-semibold mb-2">{title}</h3>
-      <p className="text-gray-600 mb-2">{description}</p>
+      <h3 className="text-xl font-semibold mb-0 line-clamp-2 h-[4.5rem]">{title}</h3>
+      <p className="text-gray-600 mb-2 ">{description}</p>
+      <div className="flex justify-between">
       <span className="text-yellow-500 font-bold">{price}</span>
+      <button type="submit"  className =" bg-gray-900 border border-solid border-white" onClick={handleAddToCart}>+</button>
+      </div>
     </div>
   );
 };

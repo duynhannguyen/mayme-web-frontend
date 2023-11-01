8c890b29f912dfdbb3c956625eba479235be0c97
duynhannguyen <130212806+duynhannguyen@users.noreply.github.com>
2023-11-02 05:29:33 +0700

    handle checkout

--- src/pages/ListMenu/ListMenu.jsx
+++ src/pages/ListMenu/ListMenu.jsx
@@ -1,48 +1,59 @@
 import React, { useEffect, useState } from "react";
 import Item from "../../component/Item/Item";
-import QRCode from "qrcode.react";
+// import QRCode from "qrcode.react";
+import QRCode from "qrcode";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchDishList } from "../../redux/DishList/dishListAction";
 import Cart from "../../component/Cart/Cart";
 const ListMenu = () => {
-  const menu2Url = "http://localhost:5173/menu2";
+  const menu2Url = "https://www.npmjs.com/package/qrcode";
   const [showQRCode, setShowQRCode] = useState(false);
   const [showCart, setShowCart] = useState(false);
   const [currentMenu, setCurrentMenu] = useState("menu");
   const [cartItems, setCartItems] = useState([]);
-  // const handleAddToCart = (product) => {
-  //   setCartItems([...cartItems, product]);
-  // };
   const dispatch = useDispatch();
   const getDishList = useSelector((state) => state.dishList.dishList);
   useEffect(() => {
     dispatch(fetchDishList());
   }, []);
 
+  const generateQR = () => {
+    return QRCode.toString(
+      menu2Url,
+      {
+        type: "image/jpeg",
+        quality: 0.3,
+        margin: 1,
+        color: {
+          dark: "#010599FF",
+          light: "#FFBF60FF",
+        },
+      },
+      (err, string) => {
+        console.log(string);
+      }
+    );
+  };
   const handleAddToCart = (id) => {
     const existingItem = getDishList.find((item) => item._id === id);
-    console.log(existingItem);
     const existingItemIndex = cartItems.findIndex(
-      (cartItem) => cartItem.id === id
+      (cartItem) => cartItem._id === id
     );
     if (existingItemIndex === -1) {
-      const updateCartItems = [...cartItems, existingItem];
-      updateCartItems[existingItemIndex] = {
-        ...updateCartItems[existingItemIndex],
-        quantity: updateCartItems[existingItemIndex].quantity + 1,
-      };
+      const addQuantityToItem = { ...existingItem, quantity: 1 };
 
+      const updateCartItems = [...cartItems, addQuantityToItem];
       setCartItems(updateCartItems);
+    } else {
+      cartItems[existingItemIndex] = {
+        ...cartItems[existingItemIndex],
+        quantity: cartItems[existingItemIndex].quantity + 1,
+      };
     }
-    // else {
-    //   setCartItems([...cartItems, { ...product, quantity: 1 }]);
-    // }
   };
 
   const handleMenuClick = (menu) => {
     setCurrentMenu(menu);
-    // setShowQRCode(false);
-    // setShowCart(!showCart);
   };
   const renderMenuItems = () => {
     let menuData = [];
@@ -64,22 +75,10 @@ const ListMenu = () => {
       menuData = getDishList.filter((item) => item.loai === "da xay");
     }
     return menuData.map((item) => (
-      <Item
-        key={item._id}
-        {...item}
-        // key={item.maHangHoa}
-        // imageUrl={item.hinhAnh}
-        // title={item.tenHang}
-        // description={item.nhomHang}
-        // price={item.giaBan}
-        onAddToCart={handleAddToCart}
-      />
+      <Item key={item._id} {...item} onAddToCart={handleAddToCart} />
     ));
   };
   const handleShowQRCode = () => {
-    setShowQRCode(!showQRCode);
-  };
-  const handleNotShowQRCode = () => {
     setShowQRCode(!showQRCode);
   };
   const handleShowCart = () => {
@@ -171,6 +170,10 @@ const ListMenu = () => {
                   className="h-6 w-6 "
                 />
               </div>
+              <button type="button" onClick={generateQR}>
+                {" "}
+                Tạo mã QR
+              </button>
             </div>
           </div>
           <div className="mb-10 sm:mb-0 mt-10 grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
@@ -180,7 +183,20 @@ const ListMenu = () => {
       </div>
       {showQRCode && (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-80">
-          <QRCode className="max-w-80vw max-h-80vh" value={menu2Url} />
+          {/* <QRCode className="max-w-80vw max-h-80vh" value={menu2Url} /> */}
+          {/* {generateQR()} */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 31 31"
+            shape-rendering="crispEdges"
+          >
+            <path fill="#FFBF60" d="M0 0h31v31H0z" />
+            <path
+              stroke="#010599"
+              d="M1 1.5h7m2 0h3m1 0h1m1 0h1m3 0h2m1 0h7M1 2.5h1m5 0h1m2 0h1m2 0h5m1 0h1m1 0h1m1 0h1m5 0h1M1 3.5h1m1 0h3m1 0h1m1 0h1m1 0h1m2 0h1m1 0h2m1 0h3m1 0h1m1 0h3m1 0h1M1 4.5h1m1 0h3m1 0h1m1 0h1m1 0h3m1 0h2m1 0h1m1 0h1m2 0h1m1 0h3m1 0h1M1 5.5h1m1 0h3m1 0h1m1 0h1m7 0h1m1 0h2m2 0h1m1 0h3m1 0h1M1 6.5h1m5 0h1m1 0h2m1 0h1m3 0h1m2 0h1m1 0h1m1 0h1m5 0h1M1 7.5h7m1 0h1m1 0h1m1 0h1m1 0h1m1 0h1m1 0h1m1 0h1m1 0h7M9 8.5h1m1 0h1m3 0h3m3 0h1M1 9.5h1m1 0h5m2 0h1m1 0h2m2 0h1m1 0h1m1 0h1m2 0h5M1 10.5h2m1 0h1m4 0h2m3 0h2m1 0h1m1 0h7m3 0h1M2 11.5h1m3 0h2m5 0h7m1 0h1m1 0h1M1 12.5h1m3 0h1m2 0h1m1 0h1m1 0h3m2 0h1m2 0h5m1 0h1m1 0h1M1 13.5h1m1 0h2m1 0h2m1 0h3m3 0h1m2 0h1m1 0h1m5 0h2M3 14.5h1m7 0h1m1 0h1m3 0h5m1 0h1m1 0h1m3 0h1M1 15.5h4m2 0h2m4 0h1m2 0h4m1 0h2m1 0h1m1 0h2M1 16.5h1m1 0h4m2 0h1m1 0h1m1 0h1m1 0h1m4 0h1m1 0h1m1 0h2m2 0h1M3 17.5h5m1 0h1m1 0h3m2 0h1m1 0h1m5 0h1m1 0h2M1 18.5h3m2 0h1m1 0h1m3 0h4m1 0h1m1 0h7m1 0h1m1 0h1M1 19.5h1m2 0h4m1 0h1m1 0h1m2 0h3m2 0h1m3 0h2m2 0h1M1 20.5h1m2 0h1m5 0h1m3 0h1m5 0h2m2 0h2m2 0h1M1 21.5h1m1 0h1m1 0h3m3 0h1m3 0h4m1 0h6m1 0h3M9 22.5h1m3 0h1m3 0h1m1 0h1m1 0h1m3 0h5M1 23.5h7m2 0h2m1 0h1m2 0h3m1 0h2m1 0h1m1 0h3M1 24.5h1m5 0h1m1 0h2m2 0h1m1 0h2m4 0h1m3 0h1m2 0h1M1 25.5h1m1 0h3m1 0h1m1 0h3m1 0h1m2 0h3m2 0h5m1 0h1M1 26.5h1m1 0h3m1 0h1m1 0h2m1 0h2m1 0h1m1 0h1m1 0h3m2 0h1m1 0h4M1 27.5h1m1 0h3m1 0h1m1 0h1m4 0h1m1 0h2m4 0h7M1 28.5h1m5 0h1m4 0h2m1 0h1m1 0h1m2 0h5m1 0h1m1 0h1M1 29.5h7m1 0h1m2 0h2m4 0h3m1 0h6"
+            />
+          </svg>
+
           <button
             className="absolute top-4 right-4 text-white bg-red-500 hover:bg-red-600 py-2 px-3 rounded-md"
             onClick={handleShowQRCode}
@@ -195,6 +211,7 @@ const ListMenu = () => {
             cartItems={cartItems}
             setShowCart={setShowCart}
             showCart={showCart}
+            setCartItems={setCartItems}
           />
         </div>
       )}

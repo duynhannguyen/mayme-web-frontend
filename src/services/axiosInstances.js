3baf28e15de9993aed6c031c2bf43b6f9ab82867
duynhannguyen <130212806+duynhannguyen@users.noreply.github.com>
2023-11-04 03:02:17 +0700

    change base-api-url

--- src/services/axiosInstances.js
+++ src/services/axiosInstances.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { TOKEN_TYPES } from "../constant/constant.js";
 
-const BASE_API_URL = "http://localhost:3001/api/v1";
+const BASE_API_URL = "https://mayme-backend.onrender.com";
 
 const api = axios.create({
   baseURL: BASE_API_URL,

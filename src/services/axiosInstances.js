26a389d07eab1addd1a52fdaf3138b75b3ea0f26
duynhannguyen <130212806+duynhannguyen@users.noreply.github.com>
2023-11-04 04:19:34 +0700

    change BASE_API_URL

--- src/services/axiosInstances.js
+++ src/services/axiosInstances.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
-import { TOKEN_TYPES } from '../constant/constant.js';
+import axios from "axios";
+import { TOKEN_TYPES } from "../constant/constant.js";
 
 const SERVER_DOMAIN = import.meta.env.VITE_SERVER_DOMAIN;
-const BASE_API_URL = `${SERVER_DOMAIN}/api/v1`;
+const BASE_API_URL = "http://localhost:3001/api/v1";
 
 const api = axios.create({
   baseURL: BASE_API_URL,
@@ -12,7 +12,7 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   const accessToken = localStorage.getItem(TOKEN_TYPES.ACCESS_TOKEN);
   if (accessToken) {
-    config.headers['x-access-token'] = accessToken;
+    config.headers["x-access-token"] = accessToken;
   }
   return config;
 });
@@ -22,7 +22,7 @@ api.interceptors.response.use(
   (error) => {
     if (error.response.status === 401) {
       localStorage.removeItem(TOKEN_TYPES.ACCESS_TOKEN);
-      window.location.href = '/login';
+      window.location.href = "/login";
     }
   }
 );

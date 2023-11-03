3bb53bdc82dee7721afb4bd24c8200dcacef7748
HungTrinhIT <minhhung.it.work@gmail.com>
2023-11-03 17:25:49 +0700

    add env

--- src/services/axiosInstances.js
+++ src/services/axiosInstances.js
@@ -1,7 +1,8 @@
-import axios from "axios";
-import { TOKEN_TYPES } from "../constant/constant.js";
+import axios from 'axios';
+import { TOKEN_TYPES } from '../constant/constant.js';
 
-const BASE_API_URL = "http://localhost:3001/api/v1";
+const SERVER_DOMAIN = import.meta.env.VITE_SERVER_DOMAIN;
+const BASE_API_URL = `${SERVER_DOMAIN}/api/v1`;
 
 const api = axios.create({
   baseURL: BASE_API_URL,
@@ -11,7 +12,7 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   const accessToken = localStorage.getItem(TOKEN_TYPES.ACCESS_TOKEN);
   if (accessToken) {
-    config.headers["x-access-token"] = accessToken;
+    config.headers['x-access-token'] = accessToken;
   }
   return config;
 });
@@ -21,7 +22,7 @@ api.interceptors.response.use(
   (error) => {
     if (error.response.status === 401) {
       localStorage.removeItem(TOKEN_TYPES.ACCESS_TOKEN);
-      window.location.href = "/login";
+      window.location.href = '/login';
     }
   }
 );
